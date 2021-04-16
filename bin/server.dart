38e3f18e837f966638479fb478516f305f68dba7
monotraine <syamsudin@eklankuteams.com>
2021-04-16 21:56:59 +0700

    setup simple webservice with dart

---
+++ bin/server.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:algo/palindrom.dart';
+
+Future main() async {
+
+  var _server = await HttpServer.bind(InternetAddress.loopbackIPv4, 4040);
+
+  print('listening on localhost:${_server.port}');
+
+  await for (HttpRequest request in _server) {
+    request.response.write('result: ${findPalindrom(10, 30)}');
+    await request.response.close();
+  }
+}
