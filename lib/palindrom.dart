3d00bd46c01c5ac8a46b2a0cd5fdca9405f0d0ba
monotraine <syamsudin@eklankuteams.com>
2021-04-16 22:23:13 +0700

    add comment on palindrom function

--- lib/palindrom.dart
+++ lib/palindrom.dart
@@ -1,9 +1,9 @@
 import 'package:algo/except.dart';
 
-
 // PALINDROM ALGORITMA
 // reverse process
 int _reverse(int i) {
+  //parse i number into string then reverse it and then turn back into integer again
   return int.parse(i.toString().split('').reversed.join());
 }
 
@@ -11,13 +11,19 @@ int _reverse(int i) {
 int findPalindrom(int i, int j) {
   int _i;
   List<int> _l;
+
+  //throw error when i or j is not integer
   if(!(i is int) || !(j is int)) throw TypeFileException();
 
+  //throw error when i is equal or bigger than j
   if(j <= i) throw PalindromException();
 
+  //initiate list to containing the results
   _l = [];
   for(_i = i; _i <=j; _i++) {
+    //compare _i with _i that already reversed
     if(_i == _reverse(_i)) {
+      //put palindrom number into list
       _l.add(_i);
     }
   }
@@ -27,6 +33,7 @@ int findPalindrom(int i, int j) {
 }
 
 //palindrom runner
+//this function just runner function and catching error
 void run(i, j) {
   try {
     int _l;
