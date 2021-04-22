d102756c15539d99deeaa55fc27431b234e7525d
monotraine <syamsudin@eklankuteams.com>
2021-04-22 21:41:04 +0700

    almost

--- bin/algo.dart
+++ bin/algo.dart
@@ -1,9 +1,15 @@
 import 'package:algo/palindrom.dart' as palindrom;
 import 'package:algo/library.dart' as lib;
 
-String _n = '3A13 5X19 9Y20 2C18 1N20 3N20 1M21 1F14 9A21 3N21 0E13 5G14 8A23 9E22 3N14';
+String _n = '3A13 5X19 9Y20 2C18 1N20 3N20 1M21 1F14 9A21 3N21 0E13 5G14 8A23 9E22 3N14 3A21';
+String _u = '3A13 5X19 9Y20 2C18 1N20 3N20 1M21 1F14 9A21 3N21 0E13 5G14 8A23 9E22 3N14';
+
 
 void main(List<String> arguments) {
 //  palindrom.run(100, 510);
   lib.findUnique(_n);
+  List<int> _l;
+  _l = [1, 3, 4, 2, 1, 2, 3, 8];
+
+  //print(lib.unique(_l));
 }
--- lib/library.dart
+++ lib/library.dart
@@ -1,19 +1,50 @@
 //split string into array of string
+
+List<String> _similar(List<String> s) {
+  List<String> _l;
+  String _t;
+  int _i, _j;
+
+  _l = [];
+  for(_i = 0; _i < s.length; _i++) {
+    for(_j = 0; _j < s.length; _j++) {
+      _t = s[_i];
+      //catch similar content
+      if(_t.substring(0, 2) == s[_j].substring(0, 2) && _i != _j) {
+        _l.add(_t);
+        break;
+      }
+    }
+  }
+
+  return _l;
+}
+
+List<String> _remove(List<String> s, List<String> l) {
+  int _i;
+  List<String> _p;
+  _p = [...s];
+
+  for(_i = 0; _i < l.length; _i++) {
+    _p.remove(l[_i]);
+  }
+
+  return _p;
+}
+
 void findUnique(String s) {
   List<String> _s, _l, _p;
   _s = s.split(' ');
-  _l = [];
-  _p = [];
-  String _n, _t;
+  String _n;
   int _i, _j;
-  for(_i = 0; _i < _s.length; _i++) {
-    for(_j = 0; _j < _s.length; _j++) {
-      if(_s[_i].substring(0, 2) == _s[_j].substring(0, 2) && _i != _j) {
-        _l.add(_s[_i]);
-        break;
-      }     }
-  }
 
+  _l = _similar(_s);;
+
+  _p = _remove(_s, _l);
+
+  print(_l.join(' '));
+  print(_p.join(' '));
+  print(_s.join(' '));
   _n = _l.join(' ');
-  print(_n);
+  print([..._p, ..._l].join(' '));
 }
